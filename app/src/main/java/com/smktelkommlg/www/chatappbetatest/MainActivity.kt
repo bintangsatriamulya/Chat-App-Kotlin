5cda3b13282022bdad412a3f604e9a7781bad9f9
Bintang Satria Mulya Budi <bintangsatriamulya33@gmail.com>
2024-03-29 00:21:43 +0700

    First Update

---
+++ app/src/main/java/com/smktelkommlg/www/chatappbetatest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smktelkommlg.www.chatappbetatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/smktelkommlg/www/chatappbetatest/VerificationActivity.kt
@@ -0,0 +1,11 @@
+package com.smktelkommlg.www.chatappbetatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification)
+    }
+}
